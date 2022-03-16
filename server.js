a7aa4e335f43ff74808d34a123bbd4f4856242c2
macmullensantiago <macmullensantiago@gmail.com>
2022-03-16 20:22:13 -0300

    make it better

---
+++ server.js
@@ -0,0 +1,70 @@
+const express = express('express');
+const app = express();
+const path = require('path');
+const bodyParser = require('body-parser');
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
+const URI = process.env.MONGODB_URI || 'mogodb://localhost/database';
+const PORT = process.env.PORT || 5000;
+const DB_NAME = process.env.DB_NAME;
+
+app.use(bodyParser.urlenconded({extended: false}))
+app.use(bodyParser.json());
+
+app.get('/secret', (req, res) => res.sendFile(path.join(__dirname, 'secret.html')))
+
+app.post('/secret', (req, res) => {
+    MongoClient.connect(URI, {useNewParser: true }, (err, db) => {
+        if (err) {
+            console.log(err);
+        } else {
+            const db = client.db(DB_NAME);
+            const collection = db.collection('names')
+            const entry = {
+                name: req.body.name.toLowerCase(),
+                card: req.body.number + '__of__' + req.body.suit
+            };
+            collection.insertOne(entry, (err, result) => {
+                if(err) {
+                    console.log(err);
+                } else {
+                    res.send('Inserted into database');
+                }
+            })
+            db.cose();
+        }
+    })
+})
+
+app.get('/:param*', (req, res) => {
+    const name = req.url.slice(1).toLowerCase();
+
+    MongoClient.connect(URI, { useNewUrlParser: true }, (err, client) => {
+        if(err) {
+            console.log(err);
+        } else {
+            const db = client.db(DB_NAME)
+            const collection = db.collection('names');
+
+            if (name === 'deleteall') {
+                collection.remove({});
+                res.send('database reset');
+            } else {
+                collection.find({name: name}).toArray((err, result) => {
+                    if (err) {
+                        console.log(err);
+                    } else if (result.length) {
+                        const card = result[result.length-1]
+                        res.sendFile(path.join(__dirname) + '/cards/' + card)
+                    } else {
+                        res.sendStatus(404);
+                    )
+
+                    db.close();
+                    })
+                }
+            }
+        })
+    })
+
+    app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
